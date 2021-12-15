dfa37505d2483d1a9f8a9f4bab393df787c5f0a1
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-15 08:26:10 +0200

    add json format to pagination links

--- app/views/shared/_pagination.json.jbuilder
+++ app/views/shared/_pagination.json.jbuilder
@@ -1,11 +1,11 @@
 json._links do
-  json.first_page "#{url}?page=#{data.current_page}"
-  json.prev_page "#{url}?page=#{data.current_page - 1}" if (data.current_page > 1)
+  json.first_page "#{url}.json?page=#{data.current_page}"
+  json.prev_page "#{url}.json?page=#{data.current_page - 1}" if (data.current_page > 1)
 
-  json.current_page "#{url}?page=#{data.current_page}"
+  json.current_page "#{url}.json?page=#{data.current_page}"
 
-  json.next_page "#{url}?page=#{data.current_page + 1}" if data.next_page
-  json.last_page "#{url}?page=#{data.total_pages}"
+  json.next_page "#{url}.json?page=#{data.current_page + 1}" if data.next_page
+  json.last_page "#{url}.json?page=#{data.total_pages}"
 end
 
 json._meta do
--- spec/support/request_specs_helpers.rb
+++ spec/support/request_specs_helpers.rb
@@ -73,18 +73,18 @@ module RequestSpecsHelpers
       expect(json["data"][0]["id"] < json["data"][1]["id"]).to be_truthy
     end
 
-    expect(json["_links"]["current_page"]).to eq "#{url}?page=1"
+    expect(json["_links"]["current_page"]).to eq "#{url}.json?page=1"
     expect(json["_links"]["prev_page"]).to be_nil
-    expect(json["_links"]["next_page"]).to eq "#{url}?page=2"
+    expect(json["_links"]["next_page"]).to eq "#{url}.json?page=2"
     total_pages = (data.count.to_f / base_class.per_page).ceil
-    expect(json["_links"]["last_page"]).to eq "#{url}?page=#{total_pages}"
+    expect(json["_links"]["last_page"]).to eq "#{url}.json?page=#{total_pages}"
     expect(json["_meta"]["total_count"]).to eq data.count
     expect(json["_meta"]["total_pages"]).to eq total_pages
     expect(json["_meta"]["current_page"]).to eq 1
     expect(json["data"].length <= base_class.per_page).to be_truthy
 
     get json["_links"]["next_page"], headers: valid_headers, as: :json
-    expect(json["_links"]["prev_page"]).to eq "#{url}?page=1"
+    expect(json["_links"]["prev_page"]).to eq "#{url}.json?page=1"
     expect(json["_meta"]["current_page"]).to eq 2
 
     get json["_links"]["last_page"], headers: valid_headers, as: :json
